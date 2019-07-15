0006d10e0549102710816caa9b33c153d2f1a40f
Eugene Sheuchuk <shevchuked@tut.by>
2019-07-15 22:17:07 +0300

    the necessary libraries are installed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,19 +5,67 @@ const path = require('path');
 // Here you write different options and tell Webpack what to do
 module.exports = {
 
-  // Path to your entry point. From this file Webpack will begin his work
-  entry: './src/index.js',
+    // Path to your entry point. From this file Webpack will begin his work
+    entry: './src/index.js',
 
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
+    // Path and filename of your result bundle.
+    // Webpack will bundle all JavaScript into this file
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.js$/,
+                /* ... */
+            },
+            {
+                // Apply rule for .sass, .scss or .css files
+                test: /\.(sa|sc|c)ss$/,
 
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript
-  // minifying and other thing so let's set mode to development
-  mode: 'development'
+                // Set loaders to transform files.
+                // Loaders are applying from right to left(!)
+                // The first loader will be applied after others
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        // This loader resolves url() and @imports inside CSS
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                localIdentName: '[name]__[local]--[hash:base64:5]'
+                            }
+                        }
+                    },
+                    {
+                        // Then we apply postCSS fixes like autoprefixer and minifying
+                        loader: "postcss-loader"
+                    },
+                    {
+                        // First we transform SASS to standard CSS
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    // Default mode for Webpack is production.
+    // Depending on mode Webpack will apply different things
+    // on final bundle. For now we don't need production's JavaScript
+    // minifying and other thing so let's set mode to development
+    mode: 'development'
 };
